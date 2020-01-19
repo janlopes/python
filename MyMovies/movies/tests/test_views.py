d95cc1a007bad0a62f081563c981466165103536
Edson Jan Ferreira Lopes <takjanlopes@gmail.com>
2020-01-19 15:41:44 +0000

    Improving test view performance

--- MyMovies/movies/tests/test_views.py
+++ MyMovies/movies/tests/test_views.py
@@ -4,22 +4,27 @@ from mixer.backend.django import mixer
 from movies.models import Movie
 from datetime import datetime
 from movies.views import add, delete
+from django.test import TestCase
 import pytest
 
 @pytest.mark.django_db
-class TestViews:
+class TestViews(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestViews, cls).setUpClass()
+        mixer.blend('movies.MyFavoriteMovies')
+        cls.factory = RequestFactory()
 
     def test_add_view(self):
-        mixer.blend('movies.MyFavoriteMovies')
         path = reverse('add',  args=[1])
-        request = RequestFactory().get(path)
+        request = self.factory.get(path)
         response = add(request, 1)
         assert 'movies' in response.url and response.status_code == 302
 
     def test_delete_view(self):
-        mixer.blend('movies.MyFavoriteMovies')
         path = reverse('delete',  args=[1])
-        request = RequestFactory().get(path)
+        request = self.factory.get(path)
         response = add(request, 1)
         print(response.url)
         assert 'movies' in response.url and response.status_code == 302
